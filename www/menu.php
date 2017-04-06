de27a0c59320aea40acaeefdf658394f784a918f
iantreanor <trei1_15@uni.worc.ac.uk>
2017-04-06 14:19:34 +0100

    upload

---
+++ www/menu.php
@@ -0,0 +1,9 @@
+<?php
+echo '	  <ul>
+  <li><a href="index.php" class="ui.btn-active"><img src="img/home.png" width="100%" height="100%"></a></li>
+  <li><a href="beers.php" class="ui.btn-active"><img src="img/beermug3.png" width="100%" height="100%"></a></li>
+  <li><a href="festival.php" class="ui.btn-active"><img src="img/festival2.png" width="100%" height="100%"></a></li>
+  <li><a href="brewery.php" class="ui.btn-active"><img src="img/brewery3.png" width="100%" height="100%"></a></li>
+  <li><a href="camera.php" class="ui.btn-active"><img src="img/camera.png" width="100%" height="100%"></a></li>
+</ul>';
+?>
