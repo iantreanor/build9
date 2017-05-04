5d07feca511be7128714cbda7f310859a52f2cdd
iantreanor <trei1_15@uni.worc.ac.uk>
2017-05-04 09:38:43 +0100

    hg

--- www/js/camera.js
+++ www/js/camera.js
@@ -8,54 +8,16 @@ function onDeviceReady() {
 }
 
 function capturePhoto() {
-// Take picture using device camera and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+	destinationType: destinationType.DATA_URL });
+}
+	
+function onPhotoDataSuccess(imageData) {
+	var image = document.getElementById('image');
+	image.style.display = 'block';
+	image.src = "data:image/jpeg;base64," + imageData;
 }
 
-//Callback function when the picture has been successfully taken
-function onPhotoDataSuccess(imageData) {                
-    // Get image handle
-    var smallImage = document.getElementById('smallImage');
-
-    // Unhide image elements
-    smallImage.style.display = 'block';
-    smallImage.src = imageData;
-}
-
-//Callback function when the picture has not been successfully taken
 function onFail(message) {
-    alert('Failed to load picture because: ' + message);
-}
-
-function movePic(file){ 
-    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError); 
-} 
-
-//Callback function when the file system uri has been resolved
-function resolveOnSuccess(entry){ 
-    var d = new Date();
-    var n = d.getTime();
-    //new file name
-    var newFileName = n + ".jpg";
-    var myFolderApp = "EasyPacking";
-
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
-    //The folder is created if doesn't exist
-    fileSys.root.getDirectory( myFolderApp,
-                    {create:true, exclusive: false},
-                    function(directory) {
-                        entry.moveTo(directory, newFileName,  successMove, resOnError);
-                    },
-                    resOnError);
-                    },
-    resOnError);
-}
-
-//Callback function when the file has been moved successfully - inserting the complete path
-function successMove(entry) {
-    //I do my insert with "entry.fullPath" as for the path
-}
-
-function resOnError(error) {
-    alert(error.code);
+      alert('Failed because: ' + message);
 }
---
+++ www/js/camera2.js
@@ -0,0 +1,61 @@
+
+var destinationType; //used sets what should be returned (image date OR file path to image for example)
+
+document.addEventListener("deviceready",onDeviceReady,false);
+
+function onDeviceReady() {
+	destinationType=navigator.camera.DestinationType;
+}
+
+function capturePhoto() {
+// Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+}
+
+//Callback function when the picture has been successfully taken
+function onPhotoDataSuccess(imageData) {                
+    // Get image handle
+    var smallImage = document.getElementById('smallImage');
+
+    // Unhide image elements
+    smallImage.style.display = 'block';
+    smallImage.src = imageData;
+}
+
+//Callback function when the picture has not been successfully taken
+function onFail(message) {
+    alert('Failed to load picture because: ' + message);
+}
+
+function movePic(file){ 
+    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError); 
+} 
+
+//Callback function when the file system uri has been resolved
+function resolveOnSuccess(entry){ 
+    var d = new Date();
+    var n = d.getTime();
+    //new file name
+    var newFileName = n + ".jpg";
+    var myFolderApp = "EasyPacking";
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
+    //The folder is created if doesn't exist
+    fileSys.root.getDirectory( myFolderApp,
+                    {create:true, exclusive: false},
+                    function(directory) {
+                        entry.moveTo(directory, newFileName,  successMove, resOnError);
+                    },
+                    resOnError);
+                    },
+    resOnError);
+}
+
+//Callback function when the file has been moved successfully - inserting the complete path
+function successMove(entry) {
+    //I do my insert with "entry.fullPath" as for the path
+}
+
+function resOnError(error) {
+    alert(error.code);
+}
